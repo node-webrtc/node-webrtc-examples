40a25934b97ecd1b75fe1314846927469a97fccb
Yisheng Jiang <yisheng.jiang@gmail.com>
2020-09-21 12:15:35 -0700

    chanfges

--- index.js
+++ index.js
@@ -1,52 +1,128 @@
-'use strict';
+const wrtc = require("wrtc");
+const { RTCPeerConnection, RTCSessionDescription } = wrtc;
+const { RTCAudioSink } = wrtc.nonstandard;
+const iceServers = require("./lib/iceServers");
+const connections = [];
 
-const bodyParser = require('body-parser');
-const browserify = require('browserify-middleware');
-const express = require('express');
-const { readdirSync, statSync } = require('fs');
-const { join } = require('path');
+const server = require("http").createServer(async (req, res) => {
+  if (req.method === "POST") handlePOST(req, res);
+  else handleGET(req, res);
+});
 
-const { mount } = require('./lib/server/rest/connectionsapi');
-const WebRtcConnectionManager = require('./lib/server/connections/webrtcconnectionmanager');
+async function handleGET(req, res) {
+  try {
+    const pc = new RTCPeerConnection({
+      sdpSemantics: "unified-plan",
+      RTCIceServers: iceServers,
+    });
+    connections.push(pc);
+    pc.id = connections.length;
 
-const app = express();
+    const gatheredCans = [];
+    const iceCanGatherDone = new Promise((resolve) => {
+      pc.addEventListener("icecandidate", ({ candidate }) => {
+        if (!candidate) resolve();
+        else gatheredCans.push(candidate);
+      });
+      pc.oniceconnectionstatechange = () => {
+        switch (pc.iceConnectionState) {
+          case "connected":
+          case "completed":
+            resolve();
+            break;
+          case "disconnected":
+          case "failed":
+            res.writeHead(500, "ice connection failed or disconnected");
+            return;
+        }
+      };
+    });
 
-app.use(bodyParser.json());
-
-const examplesDirectory = join(__dirname, 'examples');
-
-const examples = readdirSync(examplesDirectory).filter(path =>
-  statSync(join(examplesDirectory, path)).isDirectory());
-
-function setupExample(example) {
-  const path = join(examplesDirectory, example);
-  const clientPath = join(path, 'client.js');
-  const serverPath = join(path, 'server.js');
-
-  app.use(`/${example}/index.js`, browserify(clientPath));
-  app.get(`/${example}/index.html`, (req, res) => {
-    res.sendFile(join(__dirname, 'html', 'index.html'));
+    const { track } = pc.addTransceiver("audio").receiver;
+    const sink = new RTCAudioSink(track);
+    const dataChannel = pc.createDataChannel("frequency");
+    const writable = require("fs").createWriteStream("soundin");
+    sink.ondata = (data) => {
+      console.log(data.samples.length);
+      writable.write(Buffer.from(data.samples));
+    };
+    const offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+    await iceCanGatherDone;
+    const html = /* html */ ` 
+      <html>
+        <body>
+          <video id="local" controls></video>
+          <video id="remote" controls></video>
+          <script async>
+(async function main(){
+  const remotePeerConnection = ${JSON.stringify({
+    id: pc.id,
+    localDescription: pc.localDescription,
+    candidates: gatheredCans,
+  })};
+  const config = ${JSON.stringify({
+    sdpSemantics: "unified-plan",
+    RTCIceServers: iceServers,
+  })};
+  const localVideo = document.querySelector("#local");
+  const remoteVideo = document.querySelector("#remote");
+  const bpc = new RTCPeerConnection(config);
+  await bpc.setRemoteDescription(remotePeerConnection.localDescription);
+  const localStream = await window.navigator.mediaDevices.getUserMedia({
+    audio: true,
   });
+  remotePeerConnection.candidates.forEach((candidate) => {
+    if (candidate !== null) bpc.addIceCandidate(candidate);
+  });
+  localStream
+    .getTracks()
+    .forEach((track) => bpc.addTrack(track, localStream));
 
-  const options = require(serverPath);
-  const connectionManager = WebRtcConnectionManager.create(options);
-  mount(app, connectionManager, `/${example}`);
+  localVideo.srcObject = localStream;
 
-  return connectionManager;
+  const remoteStream = new MediaStream(
+    bpc.getReceivers().map((receiver) => receiver.track)
+  );
+  remoteVideo.srcObject = remoteStream;
+
+  const originalAnswer = await bpc.createAnswer();
+  await bpc.setLocalDescription(originalAnswer);
+
+  const { gatheredCandidates } = await fetch("http://localhost:3000/${pc.id}", {
+    method: "POST",
+    body: JSON.stringify({sdp:bpc.localDescription, id:${pc.id}}),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
+  gatheredCandidates.forEach((candidate) => {
+    if (candidate !== null) bpc.addIceCandidate(candidate);
+  }); 
+})()
+          </script>
+        </body>
+      </html>`;
+    res.end(html);
+  } catch (error) {
+    console.error(error);
+    res.writeHead(500);
+    res.end();
+  }
 }
 
-app.get('/', (req, res) => res.redirect(`${examples[0]}/index.html`));
+function handlePOST(req, res) {
+  console.log(req);
 
-const connectionManagers = examples.reduce((connectionManagers, example) => {
-  const connectionManager = setupExample(example);
-  return connectionManagers.set(example, connectionManager);
-}, new Map());
-
-const server = app.listen(3000, () => {
-  const address = server.address();
-  console.log(`http://localhost:${address.port}\n`);
-
-  server.once('close', () => {
-    connectionManagers.forEach(connectionManager => connectionManager.close());
+  let d = [];
+  req.on("data", (c) => d.push(c));
+  req.on("end", async () => {
+    const json = JSON.parse(Buffer.concat(d).toString());
+    console.log(json);
+    const pc = connections[connections.length - 1];
+    await pc.setRemoteDescription(json.sdp);
   });
-});
+}
+
+server.listen(3000);
